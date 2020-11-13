8f5b2d7417d858e30035a923b9af68c98acc0440
kevinfair1 <kevinfairaso@gmail.com>
2020-11-13 09:54:07 -0500

    version 1.0

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Espresso()
+        {
+            return View();
+        } 
+        
+        public IActionResult Cappucino()
+        {
+            return View();
+        }
+
+        public IActionResult Latte()
+        {
+            return View();
+        }
+    }
+}
+       
